a9260fe82d1725fd8c1caf6b3d84f42eea91a2d9
Chekalenko Vitaliy <chekalenkovitaliy@gmail.com>
2021-01-13 16:16:27 +0200

    UD.SACO Spring Rest l.81

--- src/main/java/com/vitaliichekalenko/spring/rest/configuration/MyConfig.java
+++ src/main/java/com/vitaliichekalenko/spring/rest/configuration/MyConfig.java
@@ -26,7 +26,7 @@ public class MyConfig {
         ComboPooledDataSource dataSource = new ComboPooledDataSource();
         try {
             dataSource.setDriverClass("com.mysql.cj.jdbc.Driver");
-            dataSource.setJdbcUrl("jdbc:mysql://localhost:3306/my_db?useSSL=false&amp;serverTimezone=UTC");
+            dataSource.setJdbcUrl("jdbc:mysql://localhost:3306/my_db?useSSL=false&serverTimezone=UTC");
             dataSource.setUser("bestuser");
             dataSource.setPassword("bestuser");
         } catch (PropertyVetoException e) {
@@ -50,7 +50,7 @@ public class MyConfig {
     }
 
     @Bean
-    public HibernateTransactionManager transactionMenager(){
+    public HibernateTransactionManager transactionManager(){
         HibernateTransactionManager transactionManager = new HibernateTransactionManager();
 
         transactionManager.setSessionFactory(sessionFactory().getObject());
